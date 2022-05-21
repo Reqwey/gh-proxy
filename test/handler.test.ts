a2b70549fda57519bace3b60c8df877f80b3835a
prprnya <nya@ruavan.com>
2022-05-22 02:30:09 +0800

    301 to home

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -11,6 +11,6 @@ describe('handle', () => {
 
   test('handle GET', async () => {
     const result = await handleRequest(new Request('/', { method: 'GET' }))
-    expect(result.status).toEqual(302)
+    expect(result.status).toEqual(301)
   })
 })
