4bfc1f6a47d332055157376e303b60488454ff85
prprnya <nya@ruavan.com>
2022-05-22 17:41:36 +0800

    Add preflight init

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,3 +7,13 @@ export const GITHUB_URL_REGEX: { [key: string]: RegExp } = {
   RAW: /^(?:https?:\/\/)?raw\.(?:github(?:usercontent)?)\.com\/.+?\/.+?\/.+?\/.+$/i,
   GIST: /^(?:https?:\/\/)?gist\.(?:github(?:usercontent)?)\.com\/.+?\/.+?\/.+$/i,
 }
+
+export const PREFLIGHT_INIT: ResponseInit = {
+  status: 204,
+  headers: {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods':
+      'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
+    'access-control-max-age': '1728000',
+  },
+}
