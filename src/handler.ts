066ac29b5483f852c646ff905f1fcbed37831df7
prprnya <nya@ruavan.com>
2022-05-22 17:41:47 +0800

    Complete handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,17 +1,47 @@
-import { GITHUB_URL_REGEX, HOMEPAGE, QUERY_KEY } from './lib/constants'
+import {
+  GITHUB_URL_REGEX,
+  HOMEPAGE,
+  QUERY_KEY,
+  PREFLIGHT_INIT,
+} from './lib/constants'
+import { formatURL } from './lib/utils'
 
 export async function handleRequest(request: Request): Promise<Response> {
   const requestURL = new URL(request.url)
   const queryURL = requestURL.searchParams.get(QUERY_KEY)
-  console.log(requestURL.pathname)
 
   if (requestURL.pathname !== '/') {
-    throw new Error('Not implemented')
+    return new Response(null, { status: 204 })
   } else if (queryURL === null) {
     return Response.redirect(`${HOMEPAGE}`, 301)
   } else if (Object.values(GITHUB_URL_REGEX).some((re) => re.test(queryURL))) {
-    return new Response(`${queryURL}`)
+    return httpHandler(request, formatURL(queryURL))
   } else {
     throw new Error(`${queryURL} is not supported`)
   }
+}
+
+async function httpHandler(request: Request, url: string): Promise<Response> {
+  const method = request.method
+  const headers = request.headers
+  const body = request.body
+
+  if (method === 'OPTIONS' && headers.has('access-control-request-headers')) {
+    return new Response(null, PREFLIGHT_INIT)
+  }
+
+  return proxy(url, {
+    method,
+    headers,
+    body,
+  })
+}
+
+async function proxy(url: string, init: RequestInit): Promise<Response> {
+  const response = await fetch(url, init)
+  const status = response.status
+
+  return new Response(response.body, {
+    status,
+  })
 }
