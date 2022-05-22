22075b106fc241a638f896a2594e8cb6dfc80479
prprnya <nya@ruavan.com>
2022-05-22 18:41:44 +0800

    Use jsDelivr to proxy raw content

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { GITHUB_URL_REGEX } from '../lib/constants'
 export function formatURL(url: URL | string): string {
   try {
     url = new URL(`${url}`)
@@ -5,5 +6,31 @@ export function formatURL(url: URL | string): string {
     url = new URL(`https://${url}`)
   }
   url.protocol = 'https:'
+  return generateResourceURL(url)
+}
+
+function generateResourceURL(url: URL): string {
+  const path = url.pathname
+
+  if (GITHUB_URL_REGEX.BLOB.test(url.href)) {
+    url.host = 'cdn.jsdelivr.net'
+    url.pathname = path.replace(
+      /\/(.+?)\/(.+?)\/(?:blob|raw)\/(.+?)\/(.+)/i,
+      'gh/$1/$2@$3/$4',
+    )
+  } else if (GITHUB_URL_REGEX.RELEASE.test(url.href)) {
+    // throw new Error('Not implemented')
+  } else if (GITHUB_URL_REGEX.RAW.test(url.href)) {
+    url.host = 'cdn.jsdelivr.net'
+    url.pathname = path.replace(
+      /\/(.+?)\/(.+?)\/(.+?)\/(.+)/i,
+      'gh/$1/$2@$3/$4',
+    )
+  } else if (GITHUB_URL_REGEX.GIST.test(url.href)) {
+    // throw new Error('Not implemented')
+  } else {
+    throw new Error('Invalid URL')
+  }
+
   return url.href
 }
